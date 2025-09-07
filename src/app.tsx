fa6ba0b443aabe2d6b259ab7f17ec982bce65e4c
Samuel Alves <samuelalves681@gmail.com>
2025-09-07 10:22:14 -0300

    Init

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Room } from "./pages/room";
+import { CreateRoom } from "./pages/create-room";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export function App() {
+
+  return (
+    <>
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+     <Routes>
+      <Route index element={<CreateRoom />} />
+      <Route path="/room/:roomId" element={<Room />} />
+     </Routes>
+     </BrowserRouter>
+     </QueryClientProvider>
+    </>
+  )
+}
---
+++ src/pages/room.tsx
@@ -0,0 +1,21 @@
+import { Navigate, useParams } from "react-router-dom";
+
+type RoomParams = {
+    roomId: string;
+}
+
+export function Room() {
+
+    const { roomId } = useParams<RoomParams>();
+
+    if (!roomId) {
+        return <Navigate replace to="/"/>
+    }
+
+    return (
+        <div>
+            <h1>Room Details</h1>
+            <p>{roomId}</p>
+        </div>
+    )
+}
